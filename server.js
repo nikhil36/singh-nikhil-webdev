42dd461416065aca2e206cc5fc5ce33e119a01ad
Nikhil Kumar Singh <nikhil36@zimbra.ccs.neu.edu>
2016-09-26 10:10:17 -0400

    Changes for heroku

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(port, ipaddress);
+console.log(process.env)
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
